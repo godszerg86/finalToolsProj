6eb7326da1a86ed43a0b813953886689516bff71
Sergei Belialov <32887986+godszerg86@users.noreply.github.com>
2018-05-16 11:59:19 -0500

    Add gulp task for ugllfy JS files

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ const cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
 const concat = require('gulp-concat');
 const babel = require('gulp-babel');
-
+const htmlreplace = require('gulp-html-replace');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
 
 gulp.task('default', ['css', 'images', 'js']);
 
@@ -27,11 +29,33 @@ gulp.task('images', () => {
         .pipe(gulp.dest('dist/images'))
 });
 
-gulp.task('js', function () {
-    gulp.src(['./src/js/resources.js', './src/js/app.js', './src/js/engine.js'])
-        .pipe(babel({
+gulp.task('js', () => {
+    pump([
+        gulp.src(['./src/js/resources.js', './src/js/app.js', './src/js/engine.js']), 
+        babel({
             presets: ['env']
+        }), 
+        concat('main.js'), 
+        uglify(), 
+        gulp.dest('./dist/js/')
+    ]);
+});
+
+
+/* decided to try html replace plug-in
+it replace this three lines:
+  <script src="js/resources.js"></script>
+    <script src="js/app.js"></script>
+    <script src="js/engine.js"></script>
+    with:
+    <script src="js/main.js"></script>
+    and copy index.html to dist/ folder
+*/
+gulp.task('html', () => {
+    gulp.src('index.html')
+        .pipe(htmlreplace({
+            'js': 'js/main.js'
         }))
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('./dist/js/'));
+        .pipe(gulp.dest('dist/'));
+
 });
