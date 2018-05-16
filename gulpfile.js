974721043f10d8cdbd71894bdff6fc66861d5ed0
Sergei Belialov <32887986+godszerg86@users.noreply.github.com>
2018-05-16 11:08:15 -0500

    Add gulp task for concat JS files into main.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,10 @@ const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
 
 
-gulp.task('default', ['css','images']);
+gulp.task('default', ['css','images','js']);
 
 
 gulp.task('css', () =>
@@ -21,4 +22,10 @@ gulp.task('images', () =>{
     gulp.src('src/images/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('js', function() {
+    gulp.src(['./src/js/resources.js','./src/js/app.js','./src/js/engine.js'])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./dist/js/'));
 });
